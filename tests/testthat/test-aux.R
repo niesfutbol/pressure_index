79143995f9b3bbf01f6af1b6c371983e60706e8f
Nepo Rojas <nepo.rojas@gmail.com>
2025-07-25 17:48:36 -0700

    ✅ Update tests.
    Usaba archivos xlsx, que no están consignados. Actualizamos para que busque archivos csv.

--- tests/testthat/test-aux.R
+++ tests/testthat/test-aux.R
@@ -1,12 +1,12 @@
 describe("obtain_files_names", {
   it("tests/data", {
-    expected <- c("Barcelona", "Liverpool")
+    expected <- c("Barcelona", "Bayer_Leverkusen", "Eintracht_Frankfurt", "Liverpool", "tijuana")
     obtained <- obtain_files_names("/workdir/tests/data")
     expect_equal(obtained, expected)
   })
   it("return just the team name", {
-    files_name_list <- c("Team Stats Barcelona.xlsx", "Team Stats Liverpool.xlsx")
-    expected <- c("Barcelona", "Liverpool")
+    files_name_list <- c("Barcelona.csv", "Bayer_Leverkusen.csv", "Eintracht_Frankfurt.csv", "Liverpool.csv", "tijuana.csv")
+    expected <- c("Barcelona", "Bayer_Leverkusen", "Eintracht_Frankfurt", "Liverpool", "tijuana")
     obtained <- .clean_files_name(files_name_list)
     expect_equal(obtained, expected)
   })
