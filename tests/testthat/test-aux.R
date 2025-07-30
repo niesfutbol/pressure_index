9d395835311ba59bfcca05c1a24fd1db41dc4109
Nepo Rojas <nepo.rojas@gmail.com>
2025-07-30 16:53:09 -0700

    🛑🧪 Test `obtain_json_files_names()`

--- tests/testthat/test-aux.R
+++ tests/testthat/test-aux.R
@@ -11,3 +11,17 @@ describe("obtain_files_names", {
     expect_equal(obtained, expected)
   })
 })
+
+describe("obtain_json_files_names", {
+  it("tests/data", {
+    expected <- c("america", "atlas", "cruz_azul")
+    obtained <- obtain_json_files_names("/workdir/tests/data")
+    expect_equal(obtained, expected)
+  })
+  it("return just the team name", {
+    files_name_list <- c("america.json", "atlas.json", "cruz_azul.json")
+    expected <- c("america", "atlas", "cruz_azul")
+    obtained <- .clean_files_name(files_name_list)
+    expect_equal(obtained, expected)
+  })
+})
